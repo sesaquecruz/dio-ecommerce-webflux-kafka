6e132ff70d6a19e99518c81a0b834cb7fa8f8d88
Sesaque Cruz <118492168+sesaquecruz@users.noreply.github.com>
2022-12-28 21:34:56 -0300

    Created kafka configurations and messages for payment

---
+++ payment/src/main/java/com/ecommerce/payment/config/KafkaConsumerConfig.java
@@ -0,0 +1,37 @@
+package com.ecommerce.payment.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableKafka
+public class KafkaConsumerConfig {
+    private final KafkaProperties kafkaProperties;
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        var config = new HashMap<String, Object>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        var factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}
